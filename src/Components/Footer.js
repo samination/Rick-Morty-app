24cc6030bcbbb9be31e42775677f2d7aa52b1061
samination <43864365+samination@users.noreply.github.com>
2021-07-20 12:18:01 +0200

    Commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <a href="" target="_blank" rel="noopener noreferrer"></a>
+        <span className="ml-1"></span>
+      </div>
+      <div className="mfs-auto">
+        <span className="mr-1"></span>
+        <a href="" target="_blank" rel="noopener noreferrer"></a>
+      </div>
+    </footer>
+  )
+}
+
+export default React.memo(Footer)
